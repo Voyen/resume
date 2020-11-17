30b04a09b839461b4f719ef1a7c033ca8b83517c
Voyen <dev@voyen.io>
2020-11-17 16:13:50 +1030

    added server upload to generate script

--- generate.sh
+++ generate.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+. ./.env
+
 asciidoctor src/andrei-sevtsenko-resume.adoc -D ./dist -o index.html
 asciidoctor-pdf src/andrei-sevtsenko-resume.adoc -a pdf-theme=pdf-theme.yml -D ./dist -o resume.pdf
+
+
+for f in ./dist/*; do
+    scp -i $SSH_KEY_LOC -r "$f" $REMOTE_USER@$REMOTE_HOST:$REMOTE_STATIC_FILES_LOC
+done
